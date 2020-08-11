6b29b4bbf9e044f7fa85ce1b9142a405806c9f46
menasehk13 <53655445+menasehk13@users.noreply.github.com>
2020-08-11 19:02:44 +0300

    new update

--- app/src/main/java/com/example/wedetapp/MainActivity.java
+++ app/src/main/java/com/example/wedetapp/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.wedetapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+import meow.bottomnavigation.MeowBottomNavigation;
 
 public class MainActivity extends AppCompatActivity {
-
+MeowBottomNavigation bottomNavigation;
+TextView appname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation=findViewById(R.id.bottom_nav);
+        appname=findViewById(R.id.name);
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_baseline_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.ic_baseline_bookmark_24));
+bottomNavigation.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+    @Override
+    public Unit invoke(MeowBottomNavigation.Model model) {
+
+        return null;
     }
+});
+bottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+    @Override
+    public Unit invoke(MeowBottomNavigation.Model model) {
+appname.setText("Name");
+        return null;
+    }
+});
+    }
+
 }
