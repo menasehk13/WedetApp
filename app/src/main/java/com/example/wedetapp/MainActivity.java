5fc8286fe2582bb52b1df9ebe33f34a263551b2f
menasehk13 <53655445+menasehk13@users.noreply.github.com>
2020-08-12 07:22:26 +0300

    working Areas

--- app/src/main/java/com/example/wedetapp/MainActivity.java
+++ app/src/main/java/com/example/wedetapp/MainActivity.java
@@ -5,21 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
-import meow.bottomnavigation.MeowBottomNavigation;
+
 
 public class MainActivity extends AppCompatActivity {
 MeowBottomNavigation bottomNavigation;
-TextView appname;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavigation=findViewById(R.id.bottom_nav);
-        appname=findViewById(R.id.name);
-        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_baseline_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_baseline_local_hotel_24));
         bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.ic_baseline_bookmark_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3,R.drawable.ic_baseline_location_on_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(4,R.drawable.ic_baseline_notifications_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(5,R.drawable.ic_baseline_account_circle_24));
 bottomNavigation.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
     @Override
     public Unit invoke(MeowBottomNavigation.Model model) {
@@ -30,7 +34,7 @@ bottomNavigation.setOnShowListener(new Function1<MeowBottomNavigation.Model, Uni
 bottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
     @Override
     public Unit invoke(MeowBottomNavigation.Model model) {
-appname.setText("Name");
+
         return null;
     }
 });
