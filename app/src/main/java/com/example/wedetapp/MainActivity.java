b41116d34dd04adaf08f4a45ba0a6280fa5fa3da
menasehk13 <53655445+menasehk13@users.noreply.github.com>
2020-08-13 13:41:25 +0300

    Added some Features

--- app/src/main/java/com/example/wedetapp/MainActivity.java
+++ app/src/main/java/com/example/wedetapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.wedetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -24,9 +26,32 @@ MeowBottomNavigation bottomNavigation;
         bottomNavigation.add(new MeowBottomNavigation.Model(3,R.drawable.ic_baseline_location_on_24));
         bottomNavigation.add(new MeowBottomNavigation.Model(4,R.drawable.ic_baseline_notifications_24));
         bottomNavigation.add(new MeowBottomNavigation.Model(5,R.drawable.ic_baseline_account_circle_24));
+        bottomNavigation.show(3,true);
 bottomNavigation.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
     @Override
     public Unit invoke(MeowBottomNavigation.Model model) {
+        Fragment fragment=null;
+        switch (model.getId()){
+            case 1:
+                fragment=new HotelFragment();
+                break;
+            case 2:
+                fragment=new Booking_Fragment();
+                break;
+            case 3:
+                fragment=new MapsFragment();
+                break;
+            case 4:
+                fragment=new NotificationFragment();
+                break;
+            case 5:
+                fragment=new ProfileFragment();
+                break;
+            default:
+        }
+        FragmentTransaction ft= getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frame,fragment);
+        ft.commit();
 
         return null;
     }
@@ -38,6 +63,7 @@ bottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model
         return null;
     }
 });
+
     }
 
 }
--- app/src/main/java/com/example/wedetapp/MapsFragment.java
+++ app/src/main/java/com/example/wedetapp/MapsFragment.java
@@ -2,46 +2,78 @@ package com.example.wedetapp;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapsFragment extends Fragment {
+import java.io.IOException;
+import java.util.List;
+
+public class MapsFragment extends Fragment implements LocationListener, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+    Location lastLocation;
+    GoogleApiClient googleApiClient;
+    LocationRequest locationRequest;
+    private GoogleMap gMap;
+    EditText editText;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
-
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            gMap=googleMap;
+if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+    if (ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+        buildGoogleApi();
+        gMap.setMyLocationEnabled(true);
+    }
+}else{
+    gMap.setMyLocationEnabled(true);
+    buildGoogleApi();
+}
         }
     };
+     private synchronized void buildGoogleApi(){
+         googleApiClient=new GoogleApiClient.Builder(getActivity())
+                 .addOnConnectionFailedListener(this)
+                 .addConnectionCallbacks(this)
+                 .addApi(LocationServices.API).build();
+                  googleApiClient.connect();
 
+     }
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,@Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_maps, container, false);
     }
 
@@ -53,5 +85,41 @@ public class MapsFragment extends Fragment {
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
         }
+
+    }
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+      locationRequest =new LocationRequest();
+      locationRequest.setInterval(100);
+      locationRequest.setFastestInterval(100);
+      locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+      if (ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+          LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,locationRequest, this);
+      }
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+         lastLocation=location;
+         LatLng latLng=new LatLng(location.getLatitude(),location.getLongitude());
+         gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+         if (googleApiClient!=null){
+             LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+         }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    public void SearchLocation(View view) {
+
     }
 }
