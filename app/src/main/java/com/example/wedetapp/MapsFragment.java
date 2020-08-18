dded707f251b238f81bcdba56cf2e68329ba229c
menasehk13 <53655445+menasehk13@users.noreply.github.com>
2020-08-18 22:35:20 +0300

    new update

--- app/src/main/java/com/example/wedetapp/MapsFragment.java
+++ app/src/main/java/com/example/wedetapp/MapsFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Criteria;
 import android.location.Geocoder;
@@ -23,6 +24,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.arsy.maps_library.MapRipple;
+import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -41,85 +44,95 @@ import com.google.android.gms.tasks.Task;
 import java.io.IOException;
 import java.util.List;
 
-public class MapsFragment extends Fragment implements LocationListener, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
-    Location lastLocation;
-    GoogleApiClient googleApiClient;
-    LocationRequest locationRequest;
-    private GoogleMap gMap;
-    EditText editText;
+public class MapsFragment extends Fragment {
+    private GoogleMap gmap;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    double currentLat = 0, currentLang = 0;
+    SupportMapFragment mapFragment;
+    FloatingActionButton search;
 
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            gMap=googleMap;
-if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
-    if (ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
-        buildGoogleApi();
-        gMap.setMyLocationEnabled(true);
-    }
-}else{
-    gMap.setMyLocationEnabled(true);
-    buildGoogleApi();
-}
-        }
-    };
-     private synchronized void buildGoogleApi(){
-         googleApiClient=new GoogleApiClient.Builder(getActivity())
-                 .addOnConnectionFailedListener(this)
-                 .addConnectionCallbacks(this)
-                 .addApi(LocationServices.API).build();
-                  googleApiClient.connect();
-
-     }
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,@Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_maps, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment =
+        mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+        search=view.findViewById(R.id.searchHotel);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+        } else {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
         }
-
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MapRipple ripple= new MapRipple(gmap,new LatLng(currentLat,currentLang),getContext());
+                      ripple.withNumberOfRipples(3);
+                      ripple.withFillColor(Color.BLUE);
+                       ripple.withStrokeColor(Color.BLACK);
+                      ripple.withStrokewidth(15);
+                       ripple.withDistance(2000);
+                      ripple.withRippleDuration(20000);
+                      ripple.withTransparency(0.5f);
+                      ripple.startRippleMapAnimation();
+            }
+        });
     }
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            gmap.setMyLocationEnabled(true);
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLat = location.getLatitude();
+                    currentLang = location.getLongitude();
+                    mapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                                // TODO: Consider calling
+                                //    ActivityCompat#requestPermissions
+                                // here to request the missing permissions, and then overriding
+                                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                //                                          int[] grantResults)
+                                // to handle the case where the user grants the permission. See the documentation
+                                // for ActivityCompat#requestPermissions for more details.
+                                return;
+                            }
+                            googleMap.setMyLocationEnabled(true);
+                         gmap=googleMap;
+                         gmap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLat,currentLang),15));
+                     }
+                 });
+             }
+            }
+        });
+    }
+
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
-      locationRequest =new LocationRequest();
-      locationRequest.setInterval(100);
-      locationRequest.setFastestInterval(100);
-      locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-      if (ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
-          LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,locationRequest, this);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+      if (requestCode==44){
+          if (grantResults.length > 0 && grantResults[0]==PackageManager.PERMISSION_GRANTED) {
+              getCurrentLocation();
+          }
       }
-    }
-    @Override
-    public void onLocationChanged(Location location) {
-         lastLocation=location;
-         LatLng latLng=new LatLng(location.getLatitude(),location.getLongitude());
-         gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
-         if (googleApiClient!=null){
-             LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
-         }
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    public void SearchLocation(View view) {
-
     }
 }
